a62b588c2f7ff357a205f9689302bca7a12eb963
EasyDaMathScientist <naimnasirprp@gmail.com>
2024-12-11 09:10:31 -0800

    Enter new repo

---
+++ Loop.js
@@ -0,0 +1,28 @@
+// loop through numbers 1 to 7, categorize weeekdays and weekends
+
+for(let i =1; i<=7 ; i++){
+    if(i<=5){
+        //weekdays
+        switch(i){
+            case 1:
+                console.log("Monday- workday");
+                break;
+            case 2:
+                console.log("Tuesday- workday");
+                break;
+            case 3:
+                console.log("Wednesday- workday");
+                break;   
+            case 4:
+                console.log("Thursday- workday");
+                break;
+            case 4:
+                console.log("Friday- workday");
+                break;              
+        }  
+        } else{
+                //weekends
+            }
+        }  
+        
+
